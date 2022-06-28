952b0489cdff774cec757daa7cbeb803016f1b92
kerk0214 <kerk0214@gmail.com>
2022-06-28 09:43:21 +0900

    reset

--- testA/src/a/A.java
+++ testA/src/a/A.java
@@ -5,6 +5,9 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hi");
+		System.out.println("Hi2");
+		System.out.println("Hi3");
+
 	}
 
 }
