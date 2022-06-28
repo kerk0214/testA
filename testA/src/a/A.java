9db061c27e2a3f413d117ffcd23dbeeb2f26d551
kerk0214 <kerk0214@gmail.com>
2022-06-28 09:48:42 +0900

    .

--- testA/src/a/A.java
+++ testA/src/a/A.java
@@ -1,13 +1,18 @@
 package a;
 
+
 public class A {
+
 	public static void main(String[] args) {
 		System.out.println("Hello");
+
 		System.out.println("Hi");
+
 		System.out.println("Hi2");
 		System.out.println("Hi3");
 		System.out.println("Hello2");
 		System.out.println("Hello3");
+
 	}
 
 }
